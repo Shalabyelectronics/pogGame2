c156956b1d236773dca0682087460c4dc287a344
shalabyelectronics <mhdshalabystar@gmail.com>
2021-10-22 23:39:57 +0300

    add bounce x coordinate when any paddle hit the ball

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from scoreboard import Scoreboard
 import time
 
 screen = Screen()
+screen.title("Pog Game V2")
 screen.tracer(0)
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
@@ -14,10 +15,10 @@ ball = Ball()
 score = Scoreboard()
 screen.update()
 screen.listen()
-screen.onkeypress(key="w", fun=right_paddle.move_up)
-screen.onkeypress(key="s", fun=right_paddle.move_down)
-screen.onkeypress(key="Up", fun=left_paddle.move_up)
-screen.onkeypress(key="Down", fun=left_paddle.move_down)
+screen.onkeypress(key="w", fun=left_paddle.move_up)
+screen.onkeypress(key="s", fun=left_paddle.move_down)
+screen.onkeypress(key="Up", fun=right_paddle.move_up)
+screen.onkeypress(key="Down", fun=right_paddle.move_down)
 
 game_is_on = True
 while game_is_on:
@@ -27,6 +28,7 @@ while game_is_on:
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
     if ball.distance(right_paddle) < 50 and ball.xcor() > 320 or ball.distance(left_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
         ball.increase_speed()
     if ball.xcor() > 380:
         ball.rest_position()
